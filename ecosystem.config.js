cda68dd3ea44079979b20c399db9ffceec39830c
Satvik-Parihar <harishparihar663@gmail.com>
2026-02-04 23:31:15 +0530

    Fix PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,15 @@ module.exports = {
     apps: [
         {
             name: "optipath-app",
-            script: "./backend/server.js",
+            script: "server.js",
+            cwd: "./backend",
             env: {
                 NODE_ENV: "production",
                 PORT: 5000
-            }
+            },
+            error_file: "~/.pm2/logs/optipath-error.log",
+            out_file: "~/.pm2/logs/optipath-out.log",
+            log_date_format: "YYYY-MM-DD HH:mm:ss Z"
         }
     ]
 };
